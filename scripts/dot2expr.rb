ffd1ff7e19daa61dbade41715ef7d59b2a11bae6
Mike Lam <lam2mo@jmu.edu>
2018-11-15 10:39:02 -0500

    Add documentation for dot conversion script

--- scripts/dot2expr.rb
+++ scripts/dot2expr.rb
@@ -1,4 +1,26 @@
 #!/usr/bin/env ruby
+#
+# Convert a computation graph produced by the "trace" tool (and stored in DOT
+# format) to real-valued expressions.
+#
+# This file is part of SHVAL. For details, see https://github.com/lam2mo/shval
+#
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
 require 'set'
 
 $ids = Set.new          # all node ids
